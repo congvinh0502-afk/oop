612cd2b2dadcdb749a67b0b2d53db7b04a030b57
Vinh <congvinh0502@gmail.com>
2026-03-27 16:34:28 +0700

    upload test

---
+++ Square.java
@@ -0,0 +1,28 @@
+package oop3bai1;
+
+public class Square {
+    private double side;
+
+    public Square() {
+    }
+
+    public Square(double side) {
+        this.side = side;
+    }
+
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) {
+        this.side = side;
+    }
+
+    public double area() {
+        return side * side;
+    }
+
+    public double perimeter() {
+        return 4 * side;
+    }
+}
